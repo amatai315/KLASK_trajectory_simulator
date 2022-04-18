dc9b01f847de1adeff3e2ab3a02e5cffdb03e34d
amatai315 <62067593+amatai315@users.noreply.github.com>
2022-04-18 21:21:23 +0900

    modify biscuit_circle and add service area

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ const board_center_x = offset + board_width / 2;
 const board_center_y = offset + board_height / 2;
 const biscuit_radius = 5;
 const goal_radius = 20;
+const service_area_radius = 50;
 
 const canvas = d3
     .select("body")
@@ -29,19 +30,25 @@ const biscuit_circle_left = canvas
     .attr("cx", board_center_x - biscuit_circle_interval)
     .attr("cy", board_center_y)
     .attr("r", biscuit_radius)
-    .attr("fill", "white");
+    .attr("stroke", "white")
+    .attr("stroke-width", "2")
+    .attr("fill", "rgba(0,0,0,0)");
 const biscuit_circle_center = canvas
     .append("circle")
     .attr("cx", board_center_x)
     .attr("cy", board_center_y)
     .attr("r", biscuit_radius)
-    .attr("fill", "white");
+    .attr("stroke", "white")
+    .attr("stroke-width", "2")
+    .attr("fill", "rgba(0,0,0,0)");
 const biscuit_circle_right = canvas
     .append("circle")
     .attr("cx", board_center_x + biscuit_circle_interval)
     .attr("cy", board_center_y)
     .attr("r", biscuit_radius)
-    .attr("fill", "white");
+    .attr("stroke", "white")
+    .attr("stroke-width", "2")
+    .attr("fill", "rgba(0,0,0,0)");
 const my_goal = canvas
     .append("circle")
     .attr("cx", board_center_x)
@@ -54,3 +61,35 @@ const opponent_goal = canvas
     .attr("cy", board_center_y - goal_board_center_interval)
     .attr("r", goal_radius)
     .attr("fill", "rgb(100,150,256)");
+const my_service_area_left = canvas
+    .append("circle")
+    .attr("cx", offset)
+    .attr("cy", offset + board_height)
+    .attr("r", service_area_radius)
+    .attr("stroke", "white")
+    .attr("stroke-width", "2")
+    .attr("fill", "rgba(0,0,0,0)");
+const my_service_area_right = canvas
+    .append("circle")
+    .attr("cx", offset + board_width)
+    .attr("cy", offset + board_height)
+    .attr("r", service_area_radius)
+    .attr("stroke", "white")
+    .attr("stroke-width", "2")
+    .attr("fill", "rgba(0,0,0,0)");
+const opponent_service_area_left = canvas
+    .append("circle")
+    .attr("cx", offset + board_width)
+    .attr("cy", offset)
+    .attr("r", service_area_radius)
+    .attr("stroke", "white")
+    .attr("stroke-width", "2")
+    .attr("fill", "rgba(0,0,0,0)");
+const opponent_service_area_right = canvas
+    .append("circle")
+    .attr("cx", offset)
+    .attr("cy", offset)
+    .attr("r", service_area_radius)
+    .attr("stroke", "white")
+    .attr("stroke-width", "2")
+    .attr("fill", "rgba(0,0,0,0)");
