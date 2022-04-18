e79f1d47e54a5cb0c2b5dfb97b5ed4ad7d880578
amatai315 <62067593+amatai315@users.noreply.github.com>
2022-04-19 01:46:45 +0900

    add function of one way trajectory

--- script.js
+++ script.js
@@ -3,21 +3,28 @@ const canvasHeight = canvasWidth * 1.4;
 const offset = 20;
 const boardWidth = 300;
 const boardHeight = boardWidth * 1.4;
+
 const biscuitCirclePositionDifferenceRaito = 0.25;
 const biscuitCircleInterval = boardWidth * biscuitCirclePositionDifferenceRaito;
 const goalBoardCenterPositionDifferenceRatio = 0.4;
 const goalBoardCenterInterval = boardHeight * goalBoardCenterPositionDifferenceRatio;
-const boardCenterX = offset + boardWidth / 2;
-const boardCenterY = offset + boardHeight / 2;
 const biscuitRadius = 5;
 const goalRadius = 20;
 const serviceAreaRadius = 50;
 const ballRadius = 8;
 
+const boardCenterX = offset + boardWidth / 2;
+const boardCenterY = offset + boardHeight / 2;
+
 const ballCollisionLeft = offset + ballRadius;
 const ballCollisionRight = offset + boardWidth - ballRadius;
 const ballCollisionTop = offset + ballRadius;
 const ballCollisionBottom = offset + boardHeight - ballRadius;
+
+let derectionPosition = {
+    x: 5,
+    y: 10
+};
 
 const canvas = d3
     .select("body")
@@ -99,6 +106,25 @@ const opponent_service_area_right = canvas
     .attr("stroke", "white")
     .attr("stroke-width", "2")
     .attr("fill", "rgba(0,0,0,0)");
+const derectionDecider = canvas
+    .append("circle")
+    .attr("cx", boardCenterX)
+    .attr("cy", offset + boardHeight * 0.7)
+    .attr("r", ballRadius * 5)
+    .attr("fill", "rgba(30,230,30, 0.5)")
+    .call(
+        d3.drag()
+            .on("drag", (event) => {
+                deleteTrajectory();
+                const ballX = Number(ball.attr("cx"));
+                const ballY = Number(ball.attr("cy"));
+                const collisionPoint =
+                    calculateCollisionPoint(ballX, ballY, event.x, event.y);
+                drawTrajectory(ballX, ballY, collisionPoint.x, collisionPoint.y);
+                derectionPosition.x = event.x - ballX;
+                derectionPosition.y = event.y - ballY;
+            })
+    );
 const ball = canvas
     .append("circle")
     .attr("cx", boardCenterX)
@@ -111,12 +137,18 @@ const ball = canvas
                 deleteTrajectory();
                 ball.attr("cx", event.x)
                     .attr("cy", event.y);
-                drawTrajectory(ball.attr("cx"), ball.attr("cy"), collision_point[0].x, collision_point[0].y);
+                derectionDecider
+                    .attr("cx", event.x)
+                    .attr("cy", event.y);
+                const collisionPoint =
+                    calculateCollisionPoint(event.x, event.y, event.x + derectionPosition.x, event.y + derectionPosition.y);
+                drawTrajectory(event.x, event.y, collisionPoint.x, collisionPoint.y);
             })
 );
 
 let collision_point = [
-    {x: ballCollisionRight, y: 100}
+    { x: ballCollisionRight, y: 90 },
+    { x: 230, y: ballCollisionTop }
 ];
 
 function drawTrajectory(startPointX, startPointY, goalPointX, goalPointY) {
@@ -131,6 +163,30 @@ function drawTrajectory(startPointX, startPointY, goalPointX, goalPointY) {
 
 function deleteTrajectory() {
     d3.selectAll(".trajectory").remove()
+}
+
+function isInboardY(y) {
+    return ballCollisionTop <= y && y <= ballCollisionBottom;
+}
+function isInboardX(x) {
+    return ballCollisionLeft <= x <= ballCollisionRight;
+}
+
+function calculateCollisionPoint(x1, y1, x2, y2) {
+    if (x1 < x2) {
+        let collisionY = y1 + (ballCollisionRight - x1) * (y2 - y1) / (x2 - x1);
+        if (isInboardY(collisionY)) return { x: ballCollisionRight, y: collisionY };
+    } else if (x2 < x1) {
+        let collisionY = y1 + (ballCollisionLeft - x1) * (y2 - y1) / (x2 - x1);
+        if (isInboardY(collisionY)) return { x: ballCollisionLeft, y: collisionY };
+    } else {
+        return y1 > y2 ? { x: x1, y: ballCollisionTop } : { x: x1, y: ballCollisionBottom };
+    }
+    if (y1 < y2) {
+        return { x: (ballCollisionBottom - y1) * (x2 - x1) / (y2 - y1) + x1, y: ballCollisionBottom };
+    } else {
+        return { x: (ballCollisionTop - y1) * (x2 - x1) / (y2 - y1) + x1, y: ballCollisionTop };
+    }
 }
 
 drawTrajectory(ball.attr("cx"), ball.attr("cy"), collision_point[0].x, collision_point[0].y);
